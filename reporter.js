67baabbb57e23d39bc654287aef24613ecc0396c
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-12-27 13:49:37 +0100

    Added reporter object module

---
+++ reporter.js
@@ -0,0 +1,18 @@
+/**
+ * Grunt JSCPD Reporter
+ *
+ * @name      grunt-jscpd-reporter
+ * @package   grunt-jscpd-reporter
+ * @author    Nils Gajsek <nils.gajsek@glanzkinder.com>
+ * @copyright 2014-2015 Nils Gajsek <nils.gajsek@glanzkinder.com>
+ * @version   1.0.0
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @link      http://www.linslin.org
+ *
+ */
+
+ //use strict -> ECMAScript5 error reporting
+'use strict';
+
+
+// ##################################################  Module define // ################################################
