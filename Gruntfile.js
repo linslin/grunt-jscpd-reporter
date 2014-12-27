5ca1a7765cac12c8a5a335fb7fe5818d06894e26
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-12-27 16:41:55 +0100

    Part of main part structure

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+/**
+ * Grunt JSCPD Reporter exporter
+ *
+ * @name      grunt-jscpd-reporter
+ * @package   grunt-jscpd-reporter
+ * @author    Nils Gajsek <nils.gajsek@glanzkinder.com>
+ * @copyright 2014-2015 Nils Gajsek <nils.gajsek@glanzkinder.com>
+ * @version   1.0.0
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @link      http://www.linslin.org
+ *
+ */
+
+//export module
+module.exports = function (grunt) {
+
+    // ########################################## Object attributes // #############################################
+
+    //use strict -> ECMAScript5 error reporting
+    'use strict';
+
+    // Load local tasks.
+    grunt.loadTasks('tasks');
+
+    // Project configuration.
+    grunt.initConfig({
+
+        jscpdreporter: {
+            sourcefile: 'mocks/output.xml',
+            output: 'report/index.html'
+        }
+    });
+
+
+    /**
+     * JSCP Reporter
+     */
+    function jscpdreporter() {
+
+        /**
+         * @var {object} config
+         */
+        var config = grunt.config.get('jscpdreporter');
+
+        console.log(grunt.config.get('jscpdreporter'));
+    }
+
+
+    // Default task.
+    grunt.registerTask('default', jscpdreporter);
+
+};
