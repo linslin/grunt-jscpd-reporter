9dece17ab5f1542915f8bc9870b2351ad014d3a7
Nils Gajsek <nils.gajsek@glanzkinder.com>
2015-01-12 00:24:27 +0100

    Up for release 0.1.1

--- templates/css/jscpd-reporter.css
+++ templates/css/jscpd-reporter.css
@@ -5,7 +5,7 @@
  * @package   grunt-jscpd-reporter
  * @author    Nils Gajsek <nils.gajsek@glanzkinder.com>
  * @copyright 2014-2015 Nils Gajsek <nils.gajsek@glanzkinder.com>
- * @version   1.0.0
+ * @version   0.1.1
  * @license   http://opensource.org/licenses/MIT MIT Public
  * @link      http://www.linslin.org
  *
